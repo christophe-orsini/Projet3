2c51efcb413cd193a6a518a404c751e661499faf
Christophe <christophe.orsini@gmail.com>
2019-10-24 13:15:54 +0200

    ajout des dependances dans le jar et correction du readle et du run

--- run.bat
+++ run.bat
@@ -2,5 +2,8 @@
 cls
 if not exist target\classes\log4j.xml (
 	mvn clean package
+	
+	echo Installation terminee. Relancer la commande 'run.bat' pour jouer.
+	pause
 )
-java -jar target\gameplaystudio-1.1.1.jar
+java -jar target\gameplaystudio-1.1.2.jar
